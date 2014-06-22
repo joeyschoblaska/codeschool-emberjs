b17bd8f8c04ace94fb733d71c17cdfdeaa427108
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-06-22 18:03:21 -0500

    move products into model/fixtures

--- app/app.js
+++ app/app.js
@@ -2,22 +2,7 @@ var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 
-App.PRODUCTS = [
-  {
-    title: "Flint",
-    price: 99,
-    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
-    isOnSale: true,
-    image: "images/products/flint.png"
-  },
-  {
-    title: "Kindling",
-    price: 249,
-    description: 'Easily combustible small sticks or twigs used for starting a fire.',
-    isOnSale: false,
-    image: "images/products/kindling.png"
-  }
-]
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 App.CONTACTS = [
   {
@@ -35,7 +20,7 @@ App.CONTACTS = [
 App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
   this.resource('products', function() {
-    this.resource('product', {path: '/:title'});
+    this.resource('product', {path: '/:product_id'});
   });
   this.resource('contacts', function() {
     this.resource('contact', {path: '/:name'});
@@ -60,24 +45,52 @@ App.ContactsIndexController = Ember.Controller.extend({
 
 App.ProductsRoute = Ember.Route.extend({
   model: function() {
-    return App.PRODUCTS;
+    return this.store.findAll("product");
   }
-})
+});
 
-App.ProductRoute = Ember.Route.extend({
-  model: function(params) {
-    return App.PRODUCTS.findBy("title", params.title);
-  }
-})
+// Ember automatically does this as long as we adhere to naming conventions
+// App.ProductRoute = Ember.Route.extend({
+//   model: function(params) {
+//     return this.store.find("product", params.product_id);
+//   }
+// });
 
 App.ContactsRoute = Ember.Route.extend({
   model: function() {
     return App.CONTACTS;
   }
-})
+});
 
 App.ContactRoute = Ember.Route.extend({
   model: function(params) {
     return App.CONTACTS.findBy("name", params.name);
   }
-})
+});
+
+App.Product = DS.Model.extend({
+  title: DS.attr("string"),
+  price: DS.attr("number"),
+  description: DS.attr("string"),
+  isOnSale: DS.attr("boolean"),
+  image: DS.attr("string")
+});
+
+App.Product.FIXTURES = [
+  {
+    id: 1,
+    title: "Flint",
+    price: 99,
+    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+    isOnSale: true,
+    image: "images/products/flint.png"
+  },
+  {
+    id: 2,
+    title: "Kindling",
+    price: 249,
+    description: 'Easily combustible small sticks or twigs used for starting a fire.',
+    isOnSale: false,
+    image: "images/products/kindling.png"
+  }
+]
