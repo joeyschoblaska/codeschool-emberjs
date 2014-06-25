ab494978364d8412dce1715dec86aefddc6e8d12
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-06-25 16:01:35 -0500

    using a component object to set properties

--- app/app.js
+++ app/app.js
@@ -169,3 +169,10 @@ App.ProductsOnsaleRoute = Ember.Route.extend({
     return this.modelFor("products").filterBy("isOnSale");
   }
 });
+
+App.ProductDetailsComponent = Ember.Component.extend({
+  reviewsCount: Ember.computed.alias("product.reviews.length"),
+  hasReviews: function() {
+    return this.get("reviewsCount") > 0;
+  }.property("reviewsCount")
+});
